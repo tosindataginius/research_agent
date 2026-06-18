06f00467f15f27f0114cd612d2bead860b569d45
tosindataginious <oluwatosinoluwaseguno@gmail.com>
2026-06-18 13:33:54 +0100

    updated

--- services/llm.py
+++ services/llm.py
@@ -10,7 +10,8 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.3,
-    max_retries=5,
+    max_retries=12,
+    timeout=60,
     google_api_key=os.getenv(
         "GOOGLE_API_KEY"
     )
